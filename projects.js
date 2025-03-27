de08c13d3b4baa83704c072c207e40fd0e54ae0e
lucas <lpfeif19@gmail.com>
2025-03-27 16:07:21 -0700

    navigation on projects page

---
+++ projects.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        id: 'dawn-and-dusk',
+        year: 2025,
+        title: 'Dawn and Dusk',
+        description: 'Sync your soul with the sun',
+        image: './images/1.1b bezel.png',
+        links: [
+            {
+                text: 'Website',
+                url: '/dawn-and-dusk/'
+            },
+            {
+                text: 'Download on App Store',
+                url: 'https://apps.apple.com/us/app/dawn-and-dusk/id6736839971'
+            }
+        ]
+    },
+    {
+        id: 'tools-cowboy',
+        year: 2024,
+        title: 'Tools a cowboy would use',
+        description: 'A collection of tools and practices that embody the spirit of the American cowboy: simple, reliable, and built to last.',
+        image: './images/tools-cowboy.jpg',
+        links: [
+            {
+                text: 'Website',
+                url: 'https://toolsacowboywoulduse.com'
+            },
+            {
+                text: 'Blog',
+                url: 'https://blog.toolsacowboywoulduse.com'
+            }
+        ]
+    },
+    {
+        id: 'kaletopia',
+        year: 2023,
+        title: 'Kaletopia',
+        description: 'A collection of essays and thoughts on technology, society, and the future.',
+        image: 'https://placehold.co/1600x1000',
+        links: [
+            {
+                text: 'Read essays',
+                url: 'https://www.kaletopia.com'
+            }
+        ]
+    }
+]; 
