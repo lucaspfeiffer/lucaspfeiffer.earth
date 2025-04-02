24e11f18769d628e60bbf789db352193e715ce34
lucas <lpfeif19@gmail.com>
2025-04-02 12:25:29 -0700

    add projects to main page

--- projects.js
+++ projects.js
@@ -4,13 +4,9 @@ const projects = [
         year: 2025,
         month: 3,
         title: 'Dawn and Dusk',
-        description: 'Sync your soul with the sun',
+        description: 'Dawn and Dusk is the best way to track sunrise and sunset. Sync your soul with the sun with two notifications before these two times of day that are fundamental for our human biology. Like a prayer bell for the sun.',
         image: './images/1.1b bezel.png',
         links: [
-            {
-                text: 'Website',
-                url: '/dawn-and-dusk/'
-            },
             {
                 text: 'Download on App Store',
                 url: 'https://apps.apple.com/us/app/dawn-and-dusk/id6736839971'
@@ -22,30 +18,26 @@ const projects = [
         year: 2024,
         month: 11,
         title: 'Local Kitchens',
-        description: 'A food tech platform bringing multiple restaurant concepts together under one roof',
+        description: 'The Local Kitchens mobile app, a multibrand ordering experience for the Local Kitchens resturants. An online hawker hall experience to discover food from beloved chefs from all over the world, serving their unique dishes in the Local Kitchens resturants.',
         image: './images/LocalKitchensMobileLanding.png',
         links: [
             {
-                text: 'Website',
-                url: 'https://www.localkitchens.com'
-            },
-            {
-                text: 'Download App',
-                url: 'https://apps.apple.com/us/app/local-kitchens/id1548693549'
+                text: 'Download on App Store',
+                url: 'https://apps.apple.com/us/app/local-kitchens/id1592013238'
             }
         ]
     },
     {
-        id: 'flavor-trippin',
+        id: 'local-kitchens-kiosk',
         year: 2024,
         month: 7,
-        title: 'Flavor Trippin\'',
-        description: 'Digital ordering experience for a micro food-hall platform',
+        title: 'Local Kitchens Kiosk',
+        description: 'The Local Kitchens in-store ordering kiosk, a multibrand ordering experience for the Local Kitchens restaurants. A modern hawker hall experience to discover food from beloved chefs from all over the world, serving their unique dishes in the Local Kitchens restaurants.',
         image: './images/iPad Pro 12.9 - Space Gray - Landscape.png',
         links: [
             {
-                text: 'Order Online',
-                url: 'https://www.flavortrippin.com'
+                text: 'Find a restaurant',
+                url: 'https://maps.apple.com/?address=San%20Francisco%20Bay%20Area,%20CA,%20United%20States&auid=17686758198201699014&ll=37.789,-122.419&lsp=9902&q=Local%20Kitchens&t=h'
             }
         ]
     },
@@ -53,14 +45,9 @@ const projects = [
         id: 'kds-training',
         year: 2023,
         month: 5,
-        title: 'KDS Training App',
-        description: 'Interactive training tool for restaurant kitchen staff',
+        title: 'KitchenOS CoPilot',
+        description: 'The Local Kitchens recipe training tool from KitchenOS, an operating system for on-demand commercial kitchens. Designed to help new cooks learn dishes quickly, and to speed up all cooks training for dish swaps and new dishes.',
         image: './images/kdstraining.png',
-        links: [
-            {
-                text: 'Case Study',
-                url: 'https://www.kdstraining.com'
-            }
-        ]
+        links: []
     }
 ]; 
